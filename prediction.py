e9108b40c4acc31b7984ac01872e61581037c1c9
gletes <165621847+gletes@users.noreply.github.com>
2024-04-30 11:59:37 +0700

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
+import pandas as pd
+
+with open('RandomForest.pkl', "rb") as model_file:
+    model = pkl.load(model_file)
+with open('oneHot_encode.pkl', "rb") as oneHot_file:
+    oneHot_encoder = pkl.load(oneHot_file)
+with open('gender_encode.pkl', "rb") as binary_file:
+    gender_encoder = pkl.load(binary_file)
+with open('scaler.pkl', "rb") as scaler_file:
+    scaler = pkl.load(scaler_file)
+
+
+def main():
+    st.title('Churn Prediction')
+
+    creditScore = st.number_input("Credit score", 300,850)
+    geo = st.selectbox("Geography",["France","Germany","Spain"])
+    gender = st.radio("Gender",["Male","Female"])
+    age = st.number_input("Age",0,100)
+    tenure = st.number_input("Tenure (in years)",0,100)
+    balance = st.number_input("Balance",min_value=0)
+    num_products = st.slider("Number of products",min_value=0,value=1)
+    cc = st.radio("Do you have a credit card?",["Yes","No"])
+    active = st.radio("Are you an active member?",["Yes","No"])
+    salary = st.number_input("Estimated salary",min_value=0)
+
+    data = {'Credit Score': float(creditScore), 'Geography': geo, 'Gender': gender, 'Age': int(age),
+           'Tenure': int(tenure), 'Balance': float(balance), 'Number of Products': int(num_products),
+           'HasCreditCard': (1 if cc == "Yes" else 0), 'IsActive': (1 if active == "Yes" else 0), 'Estimated Salary': float(salary)}
+    
+    df = pd.DataFrame([list(data.values())], columns=['Credit Score','Geography', 'Gender', 'Age','Tenure', 
+                                               'Balance', 'Number of Products','HasCreditCard', 
+                                               'IsActive', 'Estimated Salary'])
+    
+
+    df = df.replace(gender_encoder)
+    cat = df[['Geography']]
+    print(df)
+    cat_enc=pd.DataFrame(oneHot_encoder.transform(cat).toarray(),columns=oneHot_encoder.get_feature_names_out())
+    df = df.reset_index(drop=True)
+    df = pd.concat([df,cat_enc], axis=1)
+    df = df.drop(['Geography'], axis=1)
+    
+    num = ['Credit Score', 'Balance', 'Estimated Salary']
+    for col in num:
+        df[col] = scaler.transform(df[[col]])
+    print(df)
+    if st.button('Make Prediction'):
+        features=df      
+        result = make_prediction(features)
+        if result == 1:
+            output = "CHURN"
+        else:
+            output = "NOT CHURN"
+        st.success(f'The prediction is: {output}')
+
+def make_prediction(features):
+    input_array = np.array(features).reshape(1, -1)
+    prediction = model.predict(input_array)
+    return prediction[0]
+
+if __name__ == '__main__':
+    main()
